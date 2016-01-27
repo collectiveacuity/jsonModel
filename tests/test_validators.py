315f26b535feccf0cb48df8c3a5659ae6ad88e86
rcj1492 <richard@collectiveacuity.com>
2016-01-27 15:58:54 -0800

    git cap correction;

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -3,8 +3,8 @@ __created__ = '2016.01'
 
 import json
 from copy import deepcopy
-from jsonstuff.exceptions import InputValidationError
-from jsonstuff.validators import jsonModel
+from jsonmodel.exceptions import InputValidationError
+from jsonmodel.validators import jsonModel
 
 class jsonModelTests(jsonModel):
 
