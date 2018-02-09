cda5f7dfa67f0a276a54bb8697c6437cbb4e5da5
/home/rcj1492 <richard@collectiveacuity.com>
2018-02-09 03:47:24 +0000

    fixed MANIFEST.in to include utils.py file so python setup.py install works; more changes needed to exclude __pycache__ from builds;

--- setup.py
+++ setup.py
@@ -84,4 +84,7 @@ git push origin master
 
 Git Remove History: [Run as admin and pause syncing]
 git filter-branch --force --index-filter 'git rm -rf --cached --ignore-unmatch dev/*' --prune-empty --tag-name-filter cat -- --all
+
+GitHub.io Documentation:
+mkdocs gh-deploy -r origin
 '''
